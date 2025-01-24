f3515db7fd77669ced3ea47066b6dc55dcaafdfa
Om Patel <ompatel.81103@gmail.com>
2025-01-24 19:06:00 +0530

    add crud for event

--- src/graphql/resolvers/event.resolver.ts
+++ src/graphql/resolvers/event.resolver.ts
@@ -11,7 +11,7 @@ interface MyContext {
     userId: string;
 }
 
-@Resolver(() => Event)
+@Resolver()
 export class EventResolver {
     @Mutation(() => Event)
     async createEvent(
@@ -22,9 +22,12 @@ export class EventResolver {
         @Arg("userId") userId: string,
         @Ctx() ctx: MyContext
     ) {
+        if (!ctx.userId) {
+            throw new Error("Not authenticated");
+        }
         // Ensure user exists before creating an event
         const existingUser = await prisma.user.findUnique({
-            where: { id: userId }
+            where: { id: ctx.userId }
         });
 
         if (!existingUser) {
@@ -47,4 +50,91 @@ export class EventResolver {
 
         return event;
     }
+
+
+
+
+    // --------------
+
+    @Mutation(() => Event)
+    async updateEvent(
+        @Arg("id") id: string,
+        @Ctx() ctx: MyContext,
+        @Arg("title", { nullable: true }) title?: string,
+        @Arg("description", { nullable: true }) description?: string,
+        @Arg("start", { nullable: true }) start?: Date,
+        @Arg("end", { nullable: true }) end?: Date
+    ) {
+        if (!ctx.userId) {
+            throw new Error("Not authenticated");
+        }
+
+        const existingEvent = await prisma.event.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                user: true,
+            },
+        });
+
+        if (!existingEvent) {
+            throw new Error("Event not found");
+        }
+
+        if (existingEvent.userId !== ctx.userId) {
+            throw new Error("You do not have permission to edit this event");
+        }
+
+        if (start && end && start > end) {
+            throw new Error("Start date cannot be later than end date.");
+        }
+
+        const updatedEventData = {
+            title: title ?? existingEvent.title,
+            description: description ?? existingEvent.description,
+            start: start ?? existingEvent.start,
+            end: end ?? existingEvent.end,
+        };
+
+        const updatedEvent = await prisma.event.update({
+            where: { id },
+            data: updatedEventData,
+            include: { user: true },
+        });
+
+        return updatedEvent;
+    }
+
+    // ------------------
+
+    @Mutation(() => String)
+    async deleteEvent(
+        @Arg("id") id: string,
+        @Ctx() ctx: MyContext
+    ) {
+        if (!ctx.userId) {
+            throw new Error("Not authenticated");
+        }
+        if (await prisma.event.delete({ where: { id: id } })) {
+            return "Event deleted"
+        }
+        throw new Error("Unable to delete event")
+    }
+
+    @Query(() => [Event])
+    async getEvents(
+        @Ctx() ctx: MyContext
+    ) {
+        if (!ctx.userId) {
+            throw new Error("Not authenticated");
+        }
+        const events = await prisma.event.findMany({ where: { userId: ctx.userId } });
+        return events;
+    }
+
+
+
+
+
 }

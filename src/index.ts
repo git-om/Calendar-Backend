9d74acd2389e5d612fa929102fa54f82b82e5f91
Om Patel <ompatel.81103@gmail.com>
2025-01-24 12:46:07 +0530

    package.json editing 1

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ const startServer = async () => {
 
     const server = new ApolloServer({
         schema,
+        introspection: true,  // Enable introspection
         context: ({ req }) => ({ req }),
     });
 
